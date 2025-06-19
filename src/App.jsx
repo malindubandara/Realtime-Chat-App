676edc721512ebf58076a24d0b6e7ff4c06f230a
Malindu Bandara <malindubandara48@gmail.com>
2025-06-19 10:41:16 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import UserLogin from "./components/UserLogin"
+
+function App() {
+
+  return (
+    <>
+    <UserLogin/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/UserLogin.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {FaReact} from 'react-icons/fa'
+import '../style.css'
+
+const UserLogin = () => {
+  return (
+    <div  className='login-container'>
+        <div className='login-title'>
+            <FaReact/>
+            <h1>Chat App</h1>
+        </div>
+        <div className='login-form'>
+            <input type="text" placeholder='Enter your name'/>
+            <button>Login</button>
+        </div>
+    </div>
+
+  )
+}
+
+export default UserLogin
